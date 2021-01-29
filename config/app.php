f122f37afa119b2b5d667257217ced03d2e7757c
NaimBiswas <nayeembiswas2@gmail.com>
2021-01-29 23:21:40 +0600

    config passport

--- config/app.php
+++ config/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use Laravel\Passport\PassportServiceProvider;
+
 return [
 
     /*
@@ -174,6 +176,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
     ],
 
